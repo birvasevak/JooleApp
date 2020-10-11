222c5156b3f3e4c7651e5061f7cc1c61960de918
birvasevak <birva.sevak001@umb.edu>
2020-10-10 20:39:13 -0400

    changed logo size

--- JooleApp.UI/App_Start/BundleConfig.cs
+++ JooleApp.UI/App_Start/BundleConfig.cs
@@ -25,6 +25,11 @@ namespace JooleApp.UI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Search/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/JooleStyles/SearchPage.css"));
         }
     }
 }
