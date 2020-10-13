9b33e6e8e5a7d01852df19465d9fce99cca1f047
birvasevak <birva.sevak001@umb.edu>
2020-10-13 13:37:12 -0400

    merged login page and updated DB

--- JooleApp.Domain/tblCategory.cs
+++ JooleApp.Domain/tblCategory.cs
@@ -12,7 +12,6 @@ namespace JooleApp.Domain
     using System;
     using System.Collections.Generic;
     
-    
     public partial class tblCategory
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -26,7 +25,5 @@ namespace JooleApp.Domain
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tblSubCategory> tblSubCategories { get; set; }
-
-        public virtual List<tblSubCategory> subCategories { get; set; }
     }
 }
